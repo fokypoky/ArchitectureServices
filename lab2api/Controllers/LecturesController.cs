c907ac41e6747429aecd5860b87dd43768e6d9aa
Anton Petukhov <petuhova805@gmail.com>
2023-11-30 13:58:11 +0300

    lab 2

--- lab2api/Controllers/LecturesController.cs
+++ lab2api/Controllers/LecturesController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Text.Json;
 using System.Text.Json.Serialization;
+using lab2api.Infrastructure.Repositories;
+using lab2api.Models;
 using lab3api.Infrastructure.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -18,19 +20,43 @@ namespace lab2api.Controllers
 			{
 				var course = context.Courses
 					.Include(c => c.Lectures)
+						.ThenInclude(l => l.Type)
 					.FirstOrDefault(c => c.Title == courseTitle);
 				if (course == null || course.Lectures.Count() == 0)
 				{
-					return NotFound();
+					return NotFound("Course or lectures not found");
 				}
+
+				var report = new List<LectureReport>();
 
 				foreach (var lecture in course.Lectures)
 				{
+					var lectureTimetables = context.Timetables
+						.Where(t => t.LectureId == lecture.Id && t.Date >= startDate && t.Date <= endDate)
+						.ToList();
 
+					int maxListeners = 0;
+
+					foreach (var timetable in lectureTimetables)
+					{
+						var qresult = new Neo4jRepository().GetVisitsCount(timetable.Date, timetable.LectureId)
+							.GetAwaiter().GetResult().ToList();
+						if (qresult[0] > maxListeners)
+						{
+							maxListeners = qresult[0];
+						}
+					}
+
+					report.Add(new LectureReport() {
+							Annotation = lecture.Annotation,
+							ListenersCount = maxListeners,
+							Type = lecture.Type.Type,
+							Requirements = lecture.Requirements
+					});
 				}
 
-				var response = JsonSerializer.Serialize(course, new JsonSerializerOptions() {ReferenceHandler = ReferenceHandler.Preserve});
-				return Ok(response);
+				var response = JsonSerializer.Serialize(report, new JsonSerializerOptions() {ReferenceHandler = ReferenceHandler.Preserve});
+				return Ok(report);
 			}
 		}
 	}
---
+++ lab2api/Infrastructure/Repositories/Neo4jRepository.cs
@@ -0,0 +1,22 @@
+﻿using Neo4jClient;
+using Neo4jClient.Cypher;
+
+namespace lab2api.Infrastructure.Repositories
+{
+	public class Neo4jRepository
+	{
+		public async Task<IEnumerable<int>> GetVisitsCount(DateTime? date, int lectureId)
+		{
+			var client = new BoltGraphClient(new Uri("bolt://localhost:1933"), "neo4j", "dr22042002");
+			await client.ConnectAsync();
+
+
+			var query = new CypherFluentQuery(client)
+				.Match("(s:Student)-[:STUDY_IN]->(g:Group)-[:VISITS]->(t:Timetable), (l:Lecture)-[:READS]->(t)")
+				.Where($"l.pg_id={lectureId} and datetime(t.date)=datetime('{date:yyyy-MM-ddTHH:mm:ss}')")
+				.Return<int>("count(distinct(s))");
+
+			return await query.ResultsAsync;
+		}
+	}
+}
