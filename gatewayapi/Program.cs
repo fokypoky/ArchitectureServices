d08b1f42abd10e63f2bded51307f039e70dff029
Anton Petukhov <petuhova805@gmail.com>
2023-12-07 00:32:41 +0300

    Allow any ip connection

--- gatewayapi/Program.cs
+++ gatewayapi/Program.cs
@@ -2,11 +2,21 @@
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
-
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy("AllowOriginPolicy", builder =>
+	{
+		builder.AllowAnyOrigin()
+			.AllowAnyMethod()
+			.AllowAnyHeader();
+	});
+});
 var app = builder.Build();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.UseCors("AllowOriginPolicy");
 
 app.UseMiddleware<JwtAuthMiddleware>();
 
