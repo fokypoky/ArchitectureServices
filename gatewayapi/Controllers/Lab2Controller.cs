802dcdacd55b6c0c84fef8f1a0f07304f4e233d5
Anton Petukhov <petuhova805@gmail.com>
2023-12-06 15:32:12 +0300

    lab2

--- gatewayapi/Controllers/Lab2Controller.cs
+++ gatewayapi/Controllers/Lab2Controller.cs
@@ -7,10 +7,27 @@ namespace gatewayapi.Controllers
 	[ApiController]
 	public class Lab2Controller : ControllerBase
 	{
-		[HttpGet]
-		public ActionResult Get(DateTime startDate, DateTime endDate, string courseTitle)
+		private readonly HttpClient _httpClient;
+		public Lab2Controller()
 		{
-			return Ok("Lab 2 controller");
+			_httpClient = new HttpClient();
+			_httpClient.BaseAddress = new Uri("http://localhost:9099/");
+		}
+
+		[HttpGet]
+		public async Task<ActionResult> Get(DateTime startDate, DateTime endDate, string courseTitle)
+		{
+			try
+			{
+				var response = await _httpClient.GetAsync($"api/lectures?startDate={startDate}&endDate={endDate}&courseTitle={courseTitle}");
+				response.EnsureSuccessStatusCode();
+				string responseBody = await response.Content.ReadAsStringAsync();
+				return Ok(responseBody);
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
 		}
 	}
 }
