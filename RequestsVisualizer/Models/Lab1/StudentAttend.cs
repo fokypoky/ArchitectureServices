068e0762ffead39fdd3879f002fe538dbb2b536f
Anton Petukhov <petuhova805@gmail.com>
2023-12-07 19:52:00 +0300

    remove attend count

--- RequestsVisualizer/Models/Lab1/StudentAttend.cs
+++ RequestsVisualizer/Models/Lab1/StudentAttend.cs
@@ -5,7 +5,7 @@
 		public string Name { get; set; }
 		public string Passbook { get; set; }
 		public string Group { get; set; }
-		public int AttendencyCount { get; set; }
+		//public int AttendencyCount { get; set; }
 		public double AttendencyPercent { get; set; }
 	}
 }
