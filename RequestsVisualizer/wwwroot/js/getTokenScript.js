1d5d29daed06812484804af326328ab0422d278a
Anton Petukhov <petuhova805@gmail.com>
2023-12-07 19:41:55 +0300

    вынесение скриптов в файлы

---
+++ RequestsVisualizer/wwwroot/js/getTokenScript.js
@@ -0,0 +1,25 @@
+var getTokenButton = document.getElementById('getToken');
+var tokenOutput = document.getElementById('tokenOutput');
+
+getTokenButton.addEventListener('click', function (event) {
+	event.preventDefault();
+
+	var login = document.getElementById('login-input').value;
+	var password = document.getElementById('password-input').value;
+
+	var url = 'http://localhost:1162/api/auth?login=' + login + '&password=' + password;
+	console.log(url);
+	fetch(url)
+		.then(response => {
+			if (!response.ok) {
+				throw new Error(response.body);
+			}
+			return response.text();
+		})
+		.then(data => {
+			tokenOutput.value = data;
+		})
+		.catch(error => {
+			tokenOutput.value = error;
+		});
+});
